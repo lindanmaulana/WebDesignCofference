ef75b7f9a3bad5eed802b961e85fa329584b952b
dian ardiansyah <you@example.com>
2024-06-03 09:52:26 +0700

    add back to top

--- src/Pages/Home/Header.jsx
+++ src/Pages/Home/Header.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import HamburgerMenu from "../../components/fragments/Home/HamburgerMenu";
 import Navigasi from "../../components/fragments/Home/Navigasi";
 import { useEffect, useState } from "react";
+import NavigasiList from "../../components/elements/NavigasiList";
 
 const Header = (props) => {
   const {herobannerRef, overviewRef, speakers, ourProgramRef, registerRef, venueRef, sponsorsRef, contactRef} = props
@@ -21,14 +22,18 @@ const Header = (props) => {
   }, [state])
   
   return (
-    <div className={`${state > 0 ? "md:bg-black fixed" : "absolute md:bg-transparent"} bg-black top-0 right-0 flexbox-center justify-between w-full py-[10px] px-10 z-50 `}>
+    <div className={`${state > 0 ? "md:bg-[#101010] fixed" : "absolute md:bg-transparent"} bg-[#101010] top-0 right-0 flexbox-center justify-between w-full py-[10px] px-10 z-50 `}>
       <div className="min-w-1/5">
-        <Link to="#" className="text-[20px] md:text-[30px] text-[#FFFFFF] p-[5px]">
-          New Event
-        </Link>
+        <Link to="#" className="text-[20px] md:text-[30px] text-[#FFFFFF] p-[5px]">New Event </Link>
       </div>
-
+      
+      {/* Navigasi mobile */}
       <HamburgerMenu />
+      
+      {/* Back to top */}
+      <ul className={` ${state > 50 ? "block" : "hidden"} fixed px-3 py-2 bg-[#101010] bottom-10 right-10 `}>
+        <NavigasiList title="𖤂" linkRef={herobannerRef} />
+      </ul>
 
       <Navigasi herobannerRef={herobannerRef} overviewRef={overviewRef} speakers={speakers} ourProgramRef={ourProgramRef} registerRef={registerRef} venueRef={venueRef} sponsorsRef={sponsorsRef} contactRef={contactRef} />
     </div>
